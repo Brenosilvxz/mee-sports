638786c3ca8a06a5801d756bda776e1dc08c1459
Brenosilvxz <josephbreno72@gmail.com>
2026-05-28 20:54:33 -0300

    post: página login

--- public/loja.js
+++ public/loja.js
@@ -1,33 +1,79 @@
+// const slider = document.querySelectorAll('.slider')
+// const btnPrev = document.getElementById('prev')
+// const btnNext = document.getElementById('next')
+
+// let currentSlide = 0;
+// function hideSlider() {
+//     slider.forEach(item => item.classList.remove('on') )
+// }
+
+// function showSlider() {
+//     slider[currentSlide].classList.add('on')
+// }
+// function nextSlider() {
+//     hideSlider
+//     if(currentSlide === slider.length -1) {
+//         currentSlide = 0
+//     } else {
+//         currentSlide++
+//     }
+//     showSlider()
+// }
+// function prevSlider() {
+//     hideSlider
+//     if(currentSlide === slider.length -1) {
+//         currentSlide = 0
+//     } else {
+//         currentSlide--
+//     }
+//     showSlider()
+// }
+
+// btnNext.addEventListener('click', () => nextSlider)
+// btnPrev.addEventListener('click', () => prevSlider)
+
+
 const slider = document.querySelectorAll('.slider')
 const btnPrev = document.getElementById('prev')
 const btnNext = document.getElementById('next')
 
-let currentSlide = 0;
+let currentSlide = 0
+
 function hideSlider() {
-    slider.forEach(item => item.classList.remove('on') )
+    slider.forEach(item => {
+        item.classList.remove('on')
+    })
 }
 
 function showSlider() {
     slider[currentSlide].classList.add('on')
 }
+
 function nextSlider() {
-    hideSlider
+
+    hideSlider()
+
     if(currentSlide === slider.length -1) {
         currentSlide = 0
     } else {
         currentSlide++
     }
-    showSlider()
-}
-function prevSlider() {
-    hideSlider
-    if(currentSlide === slider.length -1) {
-        currentSlide = 0
-    } else {
-        currentSlide--
-    }
+
     showSlider()
 }
 
-btnNext.addEventListener('click', () => nextSlider)
-btnPrev.addEventListener('click', () => prevSlider)
+function prevSlider() {
+
+    hideSlider()
+
+    if(currentSlide === 0) {
+        currentSlide = slider.length -1
+    } else {
+        currentSlide--
+    }
+
+    showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
